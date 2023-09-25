147523a468259b33790c21ef61a94bbc073a76dc
Pmarques1402 <113361390+Pmarques1402@users.noreply.github.com>
2023-09-25 08:07:30 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -1,16 +1,24 @@
 const form = document.querySelector("form")
-const rep1 = document.querySelector("h3")
-const rep1 = document.querySelector("h4")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
 
-const nome = form.inNome.value
-const nota1 = Number(form.inNota1.value)
-const nota2 = Number(form.inNota2.value)
-const media = (nota1 + nota2) /2
-resp1.innerText = 'Média das notas ${media.toFixed(2)}'
+    const nome = form.inNome.value  
+    const nota1 = Nunber(form.inNota1.value)  
+    const nota2 = Nunber(form.inNota2.value)  
+    const media = (nota1 + nota2)/2 
+    resp1.innerText = `Media das Notas ${media.toFixed(2)}`
 })
-if(media)> =7) {
-    resp.2innerText = `parabéns $(nome)! você foi aprovado(a)
+
+if(media >= 7){
+    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
     resp2.style.color = "blue"
-} else if(media> = 4) 
+} else if(media >= 4) {
+    resp2.innerText = `Atenção ${nome}! Você esta em um exame`
+    resp2.style.color = "green"
+} else{
+    resp2.innerText = `Ops ${nome}! Você foi reprovado(a)`
+    resp2.style.color = "red"
+}
